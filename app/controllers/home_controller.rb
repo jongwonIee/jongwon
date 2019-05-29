569cc4880fbf1d5a0bf93d4e391142ccfe755539
ljw3728@gmail.com <ljw3728@gmail.com>
2019-05-30 01:43:21 +0900

    add resume pdf

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def download_pdf
+    send_file(
+        "#{Rails.root}/public/resume.pdf",
+        filename: "jongwon_resume.pdf",
+        type: "application/pdf"
+    )
+  end
 end
