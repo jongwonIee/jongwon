5ca897029f813159981d80eea1fd2f06014cef1c
ljw3728@gmail.com <ljw3728@gmail.com>
2019-05-30 01:54:11 +0900

    download resume works now

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
-  get "home/download_pdf"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
