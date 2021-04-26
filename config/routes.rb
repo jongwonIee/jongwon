36d61d5a39837e2ac033970a216b637bdae52111
jongwoniee <ljw3728@gmail.com>
2021-04-27 05:13:15 +0900

    add blog url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :posts
   root 'home#index'
+
+  get '/blog' => 'posts#index'
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
